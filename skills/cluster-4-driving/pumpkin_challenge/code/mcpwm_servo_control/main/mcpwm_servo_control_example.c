c94cbb355b058b011882fdf719e418e169463684
David Kirk <dsak@bu.edu>
2019-10-31 15:21:54 -0400

    added pumpkin challenge code

--- skills/cluster-4-driving/pumpkin_challenge/code/mcpwm_servo_control/main/mcpwm_servo_control_example.c
+++ skills/cluster-4-driving/pumpkin_challenge/code/mcpwm_servo_control/main/mcpwm_servo_control_example.c
@@ -23,6 +23,92 @@
 #define STEERING_MAX_PULSEWIDTH 2100 //Maximum pulse width in microsecond
 #define STEERING_MAX_DEGREE 180 //Maximum angle in degree upto which servo can rotate
 
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "driver/gpio.h"
+
+static const int RX_BUF_SIZE = 1024;
+
+#define TXD_PIN (GPIO_NUM_17)
+#define RXD_PIN (GPIO_NUM_16)
+
+void init(void) {
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+}
+
+int sendData(const char* logName, const char* data)
+{
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
+    return txBytes;
+}
+
+static void tx_task(void *arg)
+{
+    static const char *TX_TASK_TAG = "TX_TASK";
+    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+    while (1) {
+        sendData(TX_TASK_TAG, "Hello world");
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+int rx_task()
+{
+    static const char *RX_TASK_TAG = "RX_TASK";
+    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
+    int avgDist = 0;
+    int distConcat = 0;
+
+    //while (1) {
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_RATE_MS);
+        if (rxBytes > 0) {
+            data[rxBytes] = 0;
+            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+            //ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+            //printf("%s\n", data);
+            //ESP_LOGI("test: ", "result: %s\n", data);
+            int i;
+
+            for (i = 0; i < rxBytes; i++) {
+                if (data[i] == 0x59 && data[i+1] == 0x59) {
+                    break;
+                }
+            }
+
+            for (i+=2; i < rxBytes; i+= 9) {
+                //ESP_LOGI(RX_TASK_TAG, "Lower byte %d: %x", i, data[i]);
+                //ESP_LOGI(RX_TASK_TAG, "Higher byte %d: %x", i, data[i+1]);
+                distConcat = (((uint16_t)data[i+1] << 8) | data[i]);
+                avgDist += distConcat;
+
+                ESP_LOGI(RX_TASK_TAG, "Distance: %d", distConcat);
+            }
+
+            avgDist /= rxBytes;
+        }
+
+        vTaskDelay(100/portTICK_RATE_MS);
+    //}
+    free(data);
+
+    return distConcat;
+}
+
 static void mcpwm_example_gpio_initialize(void)
 {
     printf("initializing mcpwm servo control gpio......\n");
@@ -94,18 +180,27 @@ void drive_control(void *arg)
 {
     uint32_t angle, count;
 
-    //while (1) {
+    while (1) {
         //for (count = 0; count < DRIVE_MAX_DEGREE; count++) {
         //count = 180;
         //    printf("Angle of rotation: %d\n", count);
         //    angle = drive_per_degree_init(count);
         //    printf("pulse width: %dus\n", angle);
-    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 1200);
-    vTaskDelay(1000/portTICK_RATE_MS);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
-    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 1400);
+        mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 1100);
+        vTaskDelay(100/portTICK_RATE_MS);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
+
+        int avgDist = rx_task();
+        printf("dist: %d\n", avgDist);
+
+        if (avgDist < 70) {
+            break;
+        }
 
         //}
-    //}
+    }
+
+    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 1400);
+
     vTaskDelete(NULL);
 }
 
@@ -136,6 +231,10 @@ void app_main(void)
 
     pwm_init();
     calibrateESC();
+
+    init();
+    //xTaskCreate(rx_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
+    xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
 
     printf("Testing servo motor.......\n");
     xTaskCreate(steering_control, "steering_control", 4096, NULL, 5, NULL);
