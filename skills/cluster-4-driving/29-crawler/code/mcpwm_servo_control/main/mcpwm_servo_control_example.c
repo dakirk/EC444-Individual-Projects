3e50d449eb1d4f66309625b58adaa4978c9b5d06
David Kirk <dsak@bu.edu>
2019-10-31 13:29:03 -0400

    added basic drive and steering control code

---
+++ skills/cluster-4-driving/29-crawler/code/mcpwm_servo_control/main/mcpwm_servo_control_example.c
@@ -0,0 +1,138 @@
+/* servo motor control example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_attr.h"
+
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
+
+//You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
+#define DRIVE_MIN_PULSEWIDTH 900 //Minimum pulse width in microsecond
+#define DRIVE_MAX_PULSEWIDTH 1500 //Maximum pulse width in microsecond
+#define DRIVE_MAX_DEGREE 180 //Maximum angle in degree upto which servo can rotate
+#define STEERING_MIN_PULSEWIDTH 700 //Minimum pulse width in microsecond
+#define STEERING_MAX_PULSEWIDTH 2100 //Maximum pulse width in microsecond
+#define STEERING_MAX_DEGREE 180 //Maximum angle in degree upto which servo can rotate
+
+static void mcpwm_example_gpio_initialize(void)
+{
+    printf("initializing mcpwm servo control gpio......\n");
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, 18);    //Set GPIO 18 as PWM0A, to which drive wheels are connected
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, 19);    //Set GPIO 19 as PWM0A, to which steering servo is connected
+}
+
+/**
+ * @brief Use this function to calcute pulse width for per degree rotation
+ *
+ * @param  degree_of_rotation the angle in degree to which servo has to rotate
+ *
+ * @return
+ *     - calculated pulse width
+ */
+static uint32_t drive_per_degree_init(uint32_t degree_of_rotation)
+{
+    uint32_t cal_pulsewidth = 0;
+    cal_pulsewidth = (DRIVE_MIN_PULSEWIDTH + (((DRIVE_MAX_PULSEWIDTH - DRIVE_MIN_PULSEWIDTH) * (degree_of_rotation)) / (DRIVE_MAX_DEGREE)));
+    return cal_pulsewidth;
+}
+
+/**
+ * @brief Use this function to calcute pulse width for per degree rotation
+ *
+ * @param  degree_of_rotation the angle in degree to which servo has to rotate
+ *
+ * @return
+ *     - calculated pulse width
+ */
+static uint32_t steering_per_degree_init(uint32_t degree_of_rotation)
+{
+    uint32_t cal_pulsewidth = 0;
+    cal_pulsewidth = (STEERING_MIN_PULSEWIDTH + (((STEERING_MAX_PULSEWIDTH - STEERING_MIN_PULSEWIDTH) * (degree_of_rotation)) / (STEERING_MAX_DEGREE)));
+    return cal_pulsewidth;
+}
+
+void pwm_init() {
+    //1. mcpwm gpio initialization
+    mcpwm_example_gpio_initialize();
+
+    //2. initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm......\n");
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+}
+
+void calibrateESC() {
+    vTaskDelay(3000 / portTICK_PERIOD_MS);  // Give yourself time to turn on crawler
+    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 2100); // HIGH signal in microseconds
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 700);  // LOW signal in microseconds
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 1400); // NEUTRAL signal in microseconds
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 1400); // reset the ESC to neutral (non-moving) value
+}
+
+/**
+ * @brief Configure MCPWM module
+ */
+void drive_control(void *arg)
+{
+    uint32_t angle, count;
+
+    while (1) {
+        for (count = 0; count < DRIVE_MAX_DEGREE; count++) {
+            printf("Angle of rotation: %d\n", count);
+            angle = drive_per_degree_init(count);
+            printf("pulse width: %dus\n", angle);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, angle);
+            vTaskDelay(100/portTICK_RATE_MS);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
+        }
+    }
+}
+
+/**
+ * @brief Configure MCPWM module
+ */
+void steering_control(void *arg)
+{
+    uint32_t angle, count;
+
+    while (1) {
+
+        for (count = 0; count < STEERING_MAX_DEGREE; count++) {
+            printf("Angle of rotation: %d\n", count);
+            angle = steering_per_degree_init(count);
+            printf("pulse width: %dus\n", angle);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, angle);
+            vTaskDelay(100/portTICK_RATE_MS);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
+        }
+
+
+    }
+}
+
+void app_main(void)
+{
+
+    pwm_init();
+    calibrateESC();
+
+    printf("Testing servo motor.......\n");
+    xTaskCreate(steering_control, "steering_control", 4096, NULL, 5, NULL);
+    xTaskCreate(drive_control, "drive_control", 4096, NULL, 4, NULL);
+
+}
