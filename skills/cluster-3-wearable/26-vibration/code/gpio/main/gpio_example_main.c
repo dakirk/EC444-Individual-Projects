6322dade430ee3a6e8294b4d6e9c7ca71cae3311
David Kirk <dsak@bu.edu>
2019-11-22 23:35:17 -0500

    added picture for state diagram

--- skills/cluster-3-wearable/26-vibration/code/gpio/main/gpio_example_main.c
+++ skills/cluster-3-wearable/26-vibration/code/gpio/main/gpio_example_main.c
@@ -31,9 +31,6 @@
  *
  */
 
-#define GPIO_OUTPUT_IO_0    18
-#define GPIO_OUTPUT_IO_1    19
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
 #define GPIO_INPUT_IO_0     4
 #define GPIO_INPUT_IO_1     5
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
